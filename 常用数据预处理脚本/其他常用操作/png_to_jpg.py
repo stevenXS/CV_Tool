a47fd183ed1aa436299066cfbf18d0f2c5780093
clw5180 <623497281@qq.com>
2019-11-25 20:23:59 +0800

    ..

--- 常用数据预处理脚本/其他常用操作/png_to_jpg.py
+++ 常用数据预处理脚本/其他常用操作/png_to_jpg.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-img_path = 'D:\GC1'
+img_path = 'E:/textile/raw/20191011(deprecated)/GC1/'
 
 from_format = '.png'
 to_format = '.jpg'
@@ -10,7 +10,7 @@ files = os.listdir(img_path)
 for idx, f in enumerate(files):
     x = os.path.splitext(f)
     # 判断jpg文件
-    if x[1] == ".jpg":
+    if x[1] == from_format:
         # 重命名
         im = Image.open(os.path.join(img_path,  x[0] + from_format))
         im.save(os.path.join(img_path, x[0] + to_format))
